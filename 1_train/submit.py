c6c03a65105a36fbfeb0977263d091115bff9775
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-11-23 15:50:04 +0100

    [lab1] update submission script

--- 1_train/submit.py
+++ 1_train/submit.py
@@ -1,6 +1,11 @@
 import argparse
+import sys
+import logging
 from pathlib import Path
 from zipfile import ZipFile
+
+logging.basicConfig()
+logger = logging.getLogger("submit.py")
 
 NUM_WORDS = 8
 MODEL_NAME = "micro_kws_student"
@@ -33,7 +38,29 @@ def get_words(file):
     return words
 
 
-def gather_files(directory):
+def check(path, directory=True, allow_missing=False):
+    if (directory and path.is_dir()) or (not directory and path.is_file()):
+        return True
+    if allow_missing:
+        logger.warning("%s does not exist. Submission will be inclomplete!", path)
+    else:
+        logger.error(
+            "%s does not exist. Use --ignore-missing to create an incomplete submission.", path
+        )
+        sys.exit(1)
+
+
+def check_file(path, allow_missing=False):
+    return check(path, directory=False, allow_missing=allow_missing)
+
+
+def check_directory(path, allow_missing=False):
+    return check(path, directory=True, allow_missing=allow_missing)
+
+
+def gather_files(directory, allow_missing=False):
+    files = []
+
     student = directory / "student"
     assert student.is_dir(), f"{student} directory does not exist"
     models = directory / "models"
@@ -43,27 +70,35 @@ def gather_files(directory):
     names_path = student / "names.txt"
     assert names_path.is_file(), f"{names_path} file does not exist"
     assert count_names(names_path) > 0
+    files.append(names_path)
     words_path = student / "words.txt"
     assert words_path.is_file(), f"{words_path} file does not exist"
     words = get_words(words_path)
     assert len(words) == NUM_WORDS, f"Expected {NUM_WORDS} in student/words.txt"
+    files.append(words_path)
 
     # Code
     model_code_path = student / "model.py"
-    assert model_code_path.is_file(), f"{model_code_path} file does not exist"
+    if check_file(model_code_path, allow_missing=allow_missing):
+        files.append(model_code_path)
     callbacks_code_path = student / "callbacks.py"
-    assert callbacks_code_path.is_file(), f"{callbacks_code_path} file does not exist"
+    if check_file(callbacks_code_path, allow_missing=allow_missing):
+        files.append(callbacks_code_path)
     metrics_code_path = student / "metrics.py"
-    assert metrics_code_path.is_file(), f"{metrics_code_path} file does not exist"
+    if check_file(metrics_code_path, allow_missing=allow_missing):
+        files.append(metrics_code_path)
     estimate_code_path = student / "estimate.py"
-    assert estimate_code_path.is_file(), f"{estimate_code_path} file does not exist"
+    if check_file(estimate_code_path, allow_missing=allow_missing):
+        files.append(estimate_code_path)
 
     # Models
     words_str = "".join(words)
     model_path = models / f"{MODEL_NAME}_{words_str}.tflite"
-    assert model_path.is_file(), f"{model_path} file does not exist"
+    if check_file(model_path, allow_missing=allow_missing):
+        files.append(model_path)
     model_quantized_path = models / f"{MODEL_NAME}_{words_str}_quantized.tflite"
-    assert model_quantized_path.is_file(), f"{model_quantized_path} file does not exist"
+    if check_file(model_quantized_path, allow_missing=allow_missing):
+        files.append(model_quantized_path)
 
     # Accuracy
     # accuracy_path = directory / "accuracy.txt"
@@ -73,33 +108,33 @@ def gather_files(directory):
     # estimations_path = directory / "estimations.txt"
     # assert estimations_path.is_file(), f"{estimations_path} file does not exist"
 
-    return [
-        names_path,
-        words_path,
-        model_code_path,
-        callbacks_code_path,
-        metrics_code_path,
-        estimate_code_path,
-        model_path,
-        model_quantized_path,
-        # accuracy_path,
-        # estimations_path,
-    ]
+    return files
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--out", default="submission_1.zip")
+    parser.add_argument(
+        "--ignore-missing",
+        action="store_true",
+        help="Will turn errors into warnings, allowing to create an incomplete submission.",
+    )
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="Print which files and directories are added to the submission.",
+    )
     args = parser.parse_args()
 
     directory = Path(".")
-    files = gather_files(directory)
+    files = gather_files(directory, allow_missing=args.ignore_missing)
 
     print(f"Creating archive: {args.out}\n")
 
     with ZipFile(args.out, "w") as handle:
         for file in files:
-            print(f"Adding file: {file}")
+            if args.verbose:
+                print(f"Adding file: {file}")
             handle.write(file)
         print(f"\nDone. Please upload the {args.out} file to Moodle!")
 
